d3d42f7e9f157227fcbeaec58cb71e27a8df8f39
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-24 16:32:01 +0900

    Numeric Limits

---
+++ 01_cpp_fundamentals/numeric_limits.cpp
@@ -0,0 +1,17 @@
+import <iostream>;
+import <limits>;
+
+using namespace std;
+
+int main()
+{
+	cout << "int:" << endl;
+  cout << "Max int value: " << numeric_limits<int>::max() << endl;
+  cout << "Min int value: " << numeric_limits<int>::min() << endl;
+  cout << "Lowest int value: " << numeric_limits<int>::lowest() << endl;
+
+	cout << "double:" << endl;
+  cout << "Max double value: " << numeric_limits<double>::max() << endl;
+  cout << "Min double value: " << numeric_limits<double>::min() << endl;
+  cout << "Lowest double value: " << numeric_limits<double>::lowest() << endl;
+}
