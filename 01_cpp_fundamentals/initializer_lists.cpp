fbe94a1d9486805a4d6b1c55dd73d1a4f41dd3b6
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-25 15:25:19 +0900

    Initializer Lists

---
+++ 01_cpp_fundamentals/initializer_lists.cpp
@@ -0,0 +1,23 @@
+import <print>;
+import <initializer_list>;
+using namespace std;
+
+int sum(initializer_list<int> values)
+{
+  int total{ 0 };
+  for (int value : values) {
+    total += value;
+  }
+  return total;
+}
+
+int main()
+{
+  int a{ sum({ 1, 2, 3})};
+  int b{ sum({ 10, 20, 30, 40, 50, 60, 70}) };
+
+  println("Sum a: {}", a);
+  println("Sum b: {}", b);
+
+  // int c{ sum({ 1, 2, 3.0}) }; // Error
+}
