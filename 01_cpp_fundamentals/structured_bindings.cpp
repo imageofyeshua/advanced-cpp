eb9381f6b95ffabc928d2d37dd1928bfe431cad5
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-25 15:01:08 +0900

    Structured Bindings

---
+++ 01_cpp_fundamentals/structured_bindings.cpp
@@ -0,0 +1,23 @@
+import <print>;
+import <string>;
+using namespace std;
+
+struct Player {
+    int id;
+    string name;
+    double health;
+};
+
+int main()
+{
+  array values{11, 22, 33};
+  auto [x, y, z] { values };
+
+  println("x: {}, y: {}, z: {}", x, y, z);
+
+  Player p1{ 1, "Astra", 95.5 };
+  
+  auto [player_id, player_name, hp] { p1 };
+
+  println("ID: {}, Name: {}, Health: {}%", player_id, player_name, hp);
+}
