18a02b74dffa9af0316fe012d0cfd62048cd99cd
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-25 16:19:53 +0900

    String

---
+++ 01_cpp_fundamentals/string.cpp
@@ -0,0 +1,10 @@
+import <print>;
+import <string>;
+using namespace std;
+
+int main() 
+{
+  string myString{ "Hello, Daniel" };
+  println("The value of myString is {}", myString);
+  println("The second letter is {}", myString[1]);
+}
