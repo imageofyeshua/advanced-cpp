45ad161e614defbced541cb6abe85eca4a40b8c7
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-25 15:02:53 +0900

    Update Pair Structured Bindings

--- 01_cpp_fundamentals/structured_bindings.cpp
+++ 01_cpp_fundamentals/structured_bindings.cpp
@@ -12,12 +12,14 @@ int main()
 {
   array values{11, 22, 33};
   auto [x, y, z] { values };
-
   println("x: {}, y: {}, z: {}", x, y, z);
 
   Player p1{ 1, "Astra", 95.5 };
-  
   auto [player_id, player_name, hp] { p1 };
-
   println("ID: {}, Name: {}, Health: {}%", player_id, player_name, hp);
+
+  pair myPair{ "Hello", 7 };
+  auto [theString, theInt] { myPair };
+  println("theString: {}", theString);
+  println("theInt: {}", theInt);
 }
