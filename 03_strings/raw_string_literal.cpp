878f8d1a5fde842b44b6242e91c1388a577fcddd
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-29 14:28:38 +0900

    Raw String Literal

---
+++ 03_strings/raw_string_literal.cpp
@@ -0,0 +1,16 @@
+import <print>;
+
+using namespace std;
+
+int main()
+{
+  println("Hello \"World\"!");
+  println(R"(Hello "World"!)");
+
+  println("Line 1\nLine 2");
+  println(R"(Line 1
+Line 2)");
+
+  println(R"(Is the following a tab character? \t)");
+  println(R"-(Embedded )" characters)-");
+}
