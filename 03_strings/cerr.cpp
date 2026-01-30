a33635a310e308b958d3917e4b746bb346c2a422
Daniel Park <intheimageofyeshua@gmail.com>
2026-01-30 14:24:21 +0900

    Standard Error Console std::cerr

---
+++ 03_strings/cerr.cpp
@@ -0,0 +1,12 @@
+import <iostream>;
+import <print>;
+
+using namespace std;
+
+int main()
+{
+  int x{ 42 };
+  int y{ 84 };
+  // std::cerr streams to the standard error console
+  println(cerr, "x has value {} and y has value {}.", x, y);
+}
