dcfca9373900670ed22b9485e9cf0f79b0216b6e
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-01 00:17:14 +0900

    Vector Store

---
+++ 04_exercises/vector_store.cpp
@@ -0,0 +1,28 @@
+import <iostream>;
+import <print>;
+import <vector>;
+
+using namespace std;
+
+int main()
+{
+	// A vector to store all values.
+	vector values = { 0.0 };
+
+	// Ask the user to enter values until 0 is entered.
+	println("Input values, one at a time followed by Enter. 0 to stop:");
+	while (true) {
+		double value;
+		cin >> value;
+		if (value == 0.0) {
+			break;
+		}
+		values.push_back(value);
+	}
+
+	// Format the values in a table with a couple of columns.
+	for (auto value : values) {
+		println("{0:16e} | {0:12f} | {0:<12g} | {0:#>+12g}", value);
+	}
+}
+
