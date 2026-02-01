c1a95c556d9b3b84d508baa49c9d2f16d3cca416
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-01 14:21:55 +0900

    Tetris Folder

---
+++ 05_sdl3_demos/Tetris/build_project.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# 1. Define variables
+INF_SOURCE_01="board.cppm"
+INF_OUTPUT_01="board.o"
+
+echo "Starting the build process..."
+
+# 2. Compile the code
+# -o specifies the output filename
+g++ -std=c++23 -fmodules-ts -x c++ -c $INF_SOURCE_01 -o $INF_OUTPUT_01
+
+# 3. Check if compilation succeeded
+if [ $? -eq 0 ]; then
+    echo "-------------------------------"
+    echo "Success! Interface files generated: $INF_OUTPUT_01 $INF_OUTPUT_01"
+    echo "Building Project"
+
+    g++ -std=c++23 -fmodules-ts main.cpp \
+        board.o \
+        -o main.app -lSDL3
+
+    ./main.app
+else
+    echo "Build failed. Please check your code."
+fi
