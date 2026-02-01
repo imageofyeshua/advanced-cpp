78ab56f51753a377cde0182b4aaaced153570f6b
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-01 10:19:37 +0900

    Booleans

---
+++ 00_basics/booleans.cpp
@@ -0,0 +1,39 @@
+import <iostream>;
+import <print>;
+
+using namespace std;
+
+int main()
+{
+
+  bool red_light {false};
+  bool green_light{true};
+
+  if(red_light == true){
+    println("Stop! You Stupid...");
+  } else {
+    println("Hurry up! man...");
+  }
+
+  if(green_light){
+    println("Green Light!!!");
+  } else {
+    println("Not Green Yo!!!");
+  }
+
+  println("-----------------------------");
+
+  //sizeof()
+  println("Bool Size : {}", sizeof(bool));
+
+  //Printing out a bool
+  //1 -->> true
+  //0 -->> false
+  println("Red Light : {}", int(red_light));
+  println("Green Light : {}", int(green_light));
+
+  println("Red Light : {}", red_light);
+  println("Green Light : {}", green_light);
+
+  return 0;
+}
