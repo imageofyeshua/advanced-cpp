254046fb1ebc76beb5e2fb55ab620c3781445eb0
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-02 23:29:38 +0900

    Printing Integers in Different Format

---
+++ 00_basics/integers_in_binary.cpp
@@ -0,0 +1,31 @@
+import <print>;
+import <bitset>;
+
+using namespace std;
+
+int main(){
+
+    unsigned short int data{ 15 };
+
+    // Displaying various bases
+    // {:d} = decimal (default)
+    // {:o} = octal
+    // {:x} = hexadecimal (lowercase)
+    // {:X} = hexadecimal (uppercase)
+    // {:b} = binary (uppercas
+
+    println("data (decimal) : {}", data);
+    println("data (decimal) : {:d}", data);
+    println("data (octal) : {:o}", data);
+    println("data (hexadecimal) : {:x}", data);
+    println("data (hexadecimal) : {:X}", data);
+    println("data (binary) : {:b}", data);
+    println("data (binary) : {:16b}", data);
+    println("data (binary) : {:#16b}", data);
+
+    println("binary expression using bitset");
+    bitset<16> binary_view(data);
+    println("data (binary) : {}", binary_view.to_string());
+   
+    return 0;
+}
