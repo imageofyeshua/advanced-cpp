6f59a2928122ea798e6b2eed9b868a12f4e4506c
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-03 22:36:49 +0900

    Integral Logic Conditions

---
+++ 00_basics/integral_logic_conditions.cpp
@@ -0,0 +1,24 @@
+import <print>;
+
+using namespace std;
+
+int main(){
+
+    int condition {-5};
+
+    bool bool_condition = condition;
+
+    if (condition){
+       println("We have a {} in our variable", condition);
+    } else {
+       println("We have a {} in our variable", condition);
+    }
+
+    if (bool_condition){
+       println("We have a {} in our variable", bool_condition);
+    } else {
+       println("We have a {} in our variable", bool_condition);
+    }
+    
+    return 0;
+}
