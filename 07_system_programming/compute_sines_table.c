f2154cb6fc8a4b66478ae2b71fd52015cfb072c0
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-08 20:22:15 +0900

    Compute Sines Table

---
+++ 07_system_programming/compute_sines_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int    angle_degree;
+  double angle_radian, pi, value;
+
+  printf("\n Compute a table of the sine function\n\n");
+  pi = 4.0*atan(1.0);
+  printf("PI = %f \n\n", pi);
+  printf("Angle\tSine \n");
+
+  angle_degree = 0;
+  while (angle_degree <= 360) {
+    angle_radian = pi * angle_degree / 180.0;
+    value = sin(angle_radian);
+    printf("%3d\t%f \n", angle_degree, value);
+    angle_degree += 10;
+  }
+
+  return 0;
+}
