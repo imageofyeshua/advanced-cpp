2933bee924df28749821ba53ad8a1fddc0567a5b
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-15 18:58:30 +0900

    Null Pointer Safety

---
+++ 00_basics/null_pointer_safety.cpp
@@ -0,0 +1,45 @@
+import <print>;
+
+using namespace std;
+
+int main(){
+
+  // Verbose nullptr check
+  println("");
+  println("Verbose nullptr check: ");
+
+  int *p_number{}; // Initialized to nullptr
+                   // p_number = new int(7);
+
+  /*
+     if (!(p_number==nullptr)){
+     println("p_number points to a VALID address : {}", static_cast<void*>(p_number));
+     println("*p_number : {}", *p_number);
+     } else {
+     println("p_number points to an INVALID address.");
+     }
+     */
+
+  if (p_number) {
+    println("p_number points to a VALID address : {}", static_cast<void*>(p_number));
+    println("*p_number : {}", *p_number);
+  } else {
+    println("p_number points to an INVALID address.");
+  }
+
+  delete p_number;
+  nullptr;
+
+  // It is OK call delete on a nullptr
+  // Calling delete on a nullptr is OK
+  int *p_number1 {};
+
+  delete p_number1;	// This won't cause any problem
+                    //if p_number1 contains nullptr
+
+                    // if(p_number1 != nullptr){
+                    //     delete p_number1;
+                    // }
+
+  return 0;
+}
