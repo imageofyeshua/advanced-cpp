3de25d308f857f4b7bdb47bcdd3a92ca87ee7d7e
Daniel Park <intheimageofyeshua@gmail.com>
2026-02-21 19:31:20 +0900

    Character Manipulation

--- 00_basics/character_manipulation.cpp
+++ 00_basics/character_manipulation.cpp
@@ -1,4 +1,3 @@
-import <iostream>;
 import <print>;
 
 using namespace std;
@@ -38,89 +37,82 @@ int main(){
   }
 
   //Check if a character is blank
-  /*
-     std::cout << std::endl;
-     std::cout << "std::isblank : "<<std::endl;
-     char message[] {"Hello there. How are you doing? The sun is shining."};
-     std::cout << "message : " << message << std::endl;
+  println("");
+  println("std::isblank : ");
+  char message[] {"Hello there. How are you doing? The sun is shining."};
+  println("message : {}", message);
 
   //Find and print blank index
   size_t blank_count{};
-  for (size_t i{0} ; i < std::size(message); ++i){
-  //std::cout << "Value : " << message[i] << std::endl;
-  if(std::isblank(message[i])){
-  std::cout << "Found a blank character at index : [" << i << "]" << std::endl;
-  ++blank_count;
+  for (size_t i{0} ; i < size(message); ++i){
+    //std::cout << "Value : " << message[i] << std::endl;
+    if(isblank(message[i])){
+      println("Found a blank character at index : [{}]",i);
+      ++blank_count;
+    }
   }
-  }
-  std::cout << "In total we found " << blank_count << " blank characters."<< std::endl;
-  */
+  println("In total we found {} blank characters.", blank_count);
 
   //Check if character is lowercase or uppercase
-  /*
-     std::cout << "std::islower and std::isupper : "<<std::endl;
+  println("std::islower and std::isupper : ");
 
-     std::cout << std::endl;
-     char thought[] {"The C++ Programming Language is one of the most used on the Planet"};
-     size_t lowercase_count{};
-     size_t upppercase_count{};
+  println("");
+  char thought[] {"The C++ Programming Language is one of the most used on the Planet"};
+  size_t lowercase_count{};
+  size_t upppercase_count{};
 
   //Print original string for ease of comparison on the terminal
-  std::cout << "Original string  : " << thought << std::endl;
+  println("Original string  : {}", thought);
 
-  for( auto character : thought){
-  if(std::islower(character)){
-  std::cout <<" " << character;
-  ++lowercase_count;
+  for(auto character : thought){
+    if (islower(character)) {
+      print("{} ", character);
+      ++lowercase_count;
+    }
+    if(isupper(character)){
+      print("{} ", character);
+      ++upppercase_count;
+    }
   }
-  if(std::isupper(character)){
-  ++upppercase_count;
-  }
-  }
-  std::cout << std::endl;
-  std::cout << "Found " << lowercase_count << " lowercase characters and "
-  <<upppercase_count << " uppercase characters."<<  std::endl;
-  */
-
+  println("");
+  println("Found {} lowercase characters and {} uppercase characters.", lowercase_count, upppercase_count);
 
   //Check if a character is a digit
-  /*
-     std::cout << std::endl;
-     std::cout << "std::isdigit : "<<std::endl;
+  println("");
+  println("std::isdigit : ");
 
-     char statement[] {"Mr Hamilton owns 221 cows. That's a lot of cows! The kid exclamed."};
-     std::cout << "statement : " << statement << std::endl;
+  char statement[] {"Mr Hamilton owns 221 cows. That's a lot of cows! The kid exclamed."};
+  println("statement : {}", statement);
 
-     size_t digit_count{};
+  size_t digit_count {};
 
-     for(auto character : statement){
-     if(std::isdigit(character)){
-     std::cout << "Found digit : " << character << std::endl;
-     ++digit_count;
-     }
-     }
-     std::cout << "Found " << digit_count << " digits in the statement string" << std::endl;
-     */
+  for(auto character : statement){
+    if(isdigit(character)){
+      println("Found digit : {}", character);
+      ++digit_count;
+    }
+  }
+  println("Found {} digits in the statement", digit_count);
 
   //Turning a character to lowercase using the std::tolower() function
-  std::cout << std::endl;
-  std::cout << "std::tolwer and std::toupper: " << std::endl;
+  println("");
+  println("std::tolwer and std::toupper: ");
   char original_str[] {"Home. The feeling of belonging"};
-  char dest_str[std::size(original_str)];
+  char dest_str[size(original_str)];
 
   //Turn this to uppercase. Change the array in place
-  for(size_t i{}; i < std::size(original_str) ; ++i){
-    dest_str[i] = std::toupper(original_str[i]);
+  for(size_t i{}; i < size(original_str); ++i){
+    dest_str[i] = toupper(original_str[i]);
   }
 
-  std::cout << "Original string : " << original_str << std::endl;
-  std::cout << "Uppercase string : " << dest_str << std::endl;
+  println("Original string : {}", original_str);
+  println("Uppercase string : {}", dest_str);
 
   //Turn this to lowercase. Change the array in place
-  for(size_t i{}; i < std::size(original_str) ; ++i){
-    dest_str[i] = std::tolower(original_str[i]);
+  for(size_t i{}; i < size(original_str); ++i){
+    dest_str[i] = tolower(original_str[i]);
   }
 
-  std::cout << "Lowercase string : " << dest_str << std::endl;
+  println("Lowercase string : {}", dest_str);
   return 0;
 }
