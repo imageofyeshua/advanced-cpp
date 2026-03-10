ff4f69c5cb76c72184071c98fe642e6a06be4483
Daniel Park <intheimageofyeshua@gmail.com>
2026-03-10 10:15:59 +0900

    String Compare()

--- 00_basics/comparing_strings.cpp
+++ 00_basics/comparing_strings.cpp
@@ -40,5 +40,31 @@ int main(){
   const char hello_char_array[] {'h','e','l','l','o','\0'}; //It works right if you put the '\' yourself
   println("{} == {} : {}", hello_str, hello_char_array, (hello_str == hello_char_array));
 
+  // Compare (1)
+  // int compare( const basic_string& str ) const noexcept;(1)
+  // Compares this string to str.
+  string str1 {"Hello"};
+  string str2 {"World"};
+
+  println("str1: {}", str1);
+  println("str2: {}", str2);
+  println("str1.compare(str2) : {}", str1.compare(str2)); // negative
+  println("str2.compare(str1) : {}", str2.compare(str1)); // positive
+
+  //Compare (2)
+  //int compare( size_type pos1, size_type count1,const basic_string& str ) const; (2)
+  //Compares a  substring  of this string(from pos1, going count1 positions) to str
+  string str3 {"Hello"};
+  string str4 {"Hello World"};
+  println("Comparing 'World' to 'Hello' : {}", str4.compare(6, 5, str3)); // 1
+
+  // Compare (3)
+  // int compare( const CharT* s ) const;(4)
+  // Compares this string to the null-terminated character sequence beginning 
+  // at the character pointed to by s.
+  string str5 {"Hello"};
+  const char *message {"World"};
+  println("Comparing Hello to World : {}", str5.compare(message)); // -1
+
   return 0;
 }
